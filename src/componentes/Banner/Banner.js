de85cb4e82c671d1cfa5fd2b672992bae5d736ba
AlexAraujoJr <alexjunior@getway.com.br>
2022-11-19 17:44:28 -0300

    re upando arquivos

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"></img>
+        </header>
+    )
+}
+
+document.createElement('img')
+
+export default Banner
